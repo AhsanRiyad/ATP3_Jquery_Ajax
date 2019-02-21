5c672f724fe3df1dc6437f13b3616f6cd3352df5
AhsanRiyad <riyad298@gmail.com>
2019-02-21 12:58:58 +0600

    ajax ended

--- abc.php
+++ abc.php
@@ -1,9 +1,17 @@
 <?php
-
-	$obj = $_POST['mydata'];
 	
-	$json = json_decode($obj);
+	$obj = $_POST['mydata'];
 
-	header("content-type: application/json");	
-	echo json_encode($json);;
+	$data = json_decode($obj);
+
+	//echo $data->name;
+	//echo $data;
+
+	header("content-type: application/json"); // parsing in the js file is not required if it is written.
+
+	$json_data = json_encode($data);
+
+	echo $json_data;
+
+
 ?>
